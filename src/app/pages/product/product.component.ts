38d4b0a0d8a3aba1dcd4221485b4269ae5a5b346
andj712 <andj712@gmail.com>
2025-06-13 13:20:11 +0200

    NgClass direktiva(uokviren button crvenom)

--- src/app/pages/product/product.component.ts
+++ src/app/pages/product/product.component.ts
@@ -5,11 +5,8 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './product.component.html',
   styleUrls: ['./product.component.scss']
 })
-export class ProductComponent implements OnInit {
+export class ProductComponent {
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
+  buttonClick:boolean = true;
 
 }
